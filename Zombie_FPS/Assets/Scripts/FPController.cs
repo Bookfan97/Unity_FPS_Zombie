406b072b86678cc143b1f3e356e65fb1d3d78d0e
Noah Day <sheltielover432@gmail.com>
2020-12-14 22:15:35 -0500

    Physics

--- Zombie_FPS/Assets/Scripts/FPController.cs
+++ Zombie_FPS/Assets/Scripts/FPController.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class FPController : MonoBehaviour
 {
     [SerializeField] private float speed = 0.1f;
+
+    private Rigidbody _rigidbody;
+    private CapsuleCollider _capsuleCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = this.GetComponent<Rigidbody>();
+        _capsuleCollider = this.GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -20,8 +25,24 @@ public class FPController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            _rigidbody.AddForce(0, 300, 0);
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        transform.position += new Vector3(x,0,z) * speed;
+        transform.position += new Vector3(x * speed,0,z * speed);
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, _capsuleCollider.radius, 
+            Vector3.down, out hit, _capsuleCollider.height/2 - _capsuleCollider.radius + 0.1f))
+        {
+            return true;
+        }
+        return false;
     }
 }
