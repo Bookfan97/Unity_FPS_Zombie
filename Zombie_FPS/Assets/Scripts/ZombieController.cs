c2de0d0fbda1fc9890d5b1eae8ebf59586a29353
Noah Day <sheltielover432@gmail.com>
2020-12-19 16:31:45 -0500

    Programming Navmesh agents

--- Zombie_FPS/Assets/Scripts/ZombieController.cs
+++ Zombie_FPS/Assets/Scripts/ZombieController.cs
@@ -1,40 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieController : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int IsAttacking = Animator.StringToHash("isAttacking");
+    private static readonly int IsDead = Animator.StringToHash("isDead");
+
     // Start is called before the first frame update
     void Start()
     {
         _animator = this.GetComponent<Animator>();
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+       
+        _navMeshAgent.SetDestination(player.transform.position);
+        if (_navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance)
         {
-            _animator.SetBool("isWalking", true);
+             _animator.SetBool(IsWalking, true);
+             _animator.SetBool(IsAttacking, false);
         }
-        else if (Input.GetKey(KeyCode.R))
+        else
         {
-            _animator.SetBool("isRunning", true);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            _animator.SetBool("isAttacking", true);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            _animator.SetBool("isDead", true);
-        }
-        else 
-        {
-            _animator.SetBool("isWalking", false);
-            _animator.SetBool("isRunning", false);
-            _animator.SetBool("isAttacking", false);
+            _animator.SetBool(IsWalking, false);
+            _animator.SetBool(IsAttacking, true);
         }
     }
 }
