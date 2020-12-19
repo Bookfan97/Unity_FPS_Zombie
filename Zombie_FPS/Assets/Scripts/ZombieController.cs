f48bf47cccbecc63135327a88b01151dc41c91a8
Noah Day <sheltielover432@gmail.com>
2020-12-19 15:23:10 -0500

    Importing Zombies with Animations

---
+++ Zombie_FPS/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            _animator.SetBool("isRunning", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _animator.SetBool("isAttacking", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _animator.SetBool("isDead", true);
+        }
+        else 
+        {
+            _animator.SetBool("isWalking", false);
+            _animator.SetBool("isRunning", false);
+            _animator.SetBool("isAttacking", false);
+        }
+    }
+}
